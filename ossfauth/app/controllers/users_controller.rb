4898b6f34d7e9fb8831d149a825771ef4ffa5267
kaworu <kaworu@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-02-26 09:15:57 +0000

    bug fix

--- ossfauth/app/controllers/users_controller.rb
+++ ossfauth/app/controllers/users_controller.rb
@@ -30,17 +30,16 @@ class UsersController < ApplicationController
     if request.post?
       s = Session.find_by_session_key(cookies[:_ossfauth_session])
       (render :text => "Session error cookie= #{cookies[:_ossfauth_session]}";return) unless s
+      cookies.delete(:key => '_ossfauth_session')
+      begin
+        require 'curb'
+        c = Curl::Easy.perform "http://140.109.22.15/index.php?option=com_ofsso&controller=sso&task=logout&username=#{s.user.name}"
+      end
+      s.delete
       if params[:return_url]
         redirect_to params[:return_url] 
       else
-        #cookies.delete(:key => '372ab5bf1e2d05e2490fab9c7e8f731b')
-        cookies.delete(:key => '_ossfauth_session')
-        begin
-        require 'curb'
-        Curl::Easy.perform "http://140.109.22.15/index.php?option=com_ofsso&controller=sso&task=logout&username=#{s.user.name}"
-        end
-        s.delete
-        render :text => "Goodbye #{s.user.name}"
+        render :text => "Goodbye"
       end
     end
   end
