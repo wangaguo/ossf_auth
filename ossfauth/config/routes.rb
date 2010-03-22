72ef5ef2e1feacfe67282233619b9a7bd9fbd8f6
kaworu <kaworu@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-03-22 02:33:38 +0000

    add resources

--- ossfauth/config/routes.rb
+++ ossfauth/config/routes.rb
@@ -32,13 +32,16 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-  map.root :controller => :account, :action => :login
+  map.root :controller => :user, :action => :home
 
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  #map.connect ':controller/:action/:id'
+  #map.connect ':controller/:action/:id.:format'
+  map.resource :user 
+  map.resources :sites
+
 end
