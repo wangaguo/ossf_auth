ba1bba1f790ad50d36b541a4c43de09da020bad5
kaworu <kaworu@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-03-22 09:05:39 +0000

    add editable_column

--- ossfauth/app/models/user.rb
+++ ossfauth/app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   def self.authenticate(name, password)
     find_by_name_and_password(name, password)
   end
+  
+  def self.editable_columns
+    a = [:first_name, :last_name, :autobiography]
+    self.content_columns.select{|col|a.member? col.name.to_sym}
+  end
 end
