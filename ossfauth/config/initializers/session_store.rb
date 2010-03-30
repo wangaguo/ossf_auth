ad4dbe90621a2864505287b390bf2ef425a19a00
kaworu <kaworu@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-03-30 07:40:48 +0000

    use memcached as session store

--- ossfauth/config/initializers/session_store.rb
+++ ossfauth/config/initializers/session_store.rb
@@ -1,15 +1,9 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_ossfauth_session',
-  :secret      => '6368683de659130ae1f20f1f007e3d3ea318a57d45167acbe517251b12301478bd7456a2fa0d6d93221b6f5b37db9bdba5836eedc06e1e5ad3f4dcd30bf96a86'
+  :key         => SITE_SESSION_ID.to_s,
+  :secret      => '30d096717228c2c04e0736dd1692a61ac96d9e11f031ddfb1d404d7edfc28ae81a14b9c',
+  :namespace   => "ofauth-#{RAILS_ENV}",
+  :memcache_server => '127.0.0.1:11211'
+
 }
 
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+ActionController::Base.session_store = :mem_cache_store
