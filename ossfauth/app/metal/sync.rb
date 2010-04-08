39fad06f50cdd3d9da5f570e3dc4f2cbe9de016e
kaworu <kaworu@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-04-08 08:37:33 +0000

    generate User json file

--- ossfauth/app/metal/sync.rb
+++ ossfauth/app/metal/sync.rb
@@ -1,10 +1,10 @@
 # Allow the metal piece to run in isolation
 require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Rails)
-
+require 'active_record'
 class Sync
   def self.call(env)
     if env["PATH_INFO"] =~ /^\/sso\/sync\.js/
-      [200, {"Content-Type" => "application/json"}, User.all.to_json]
+      [200, {"Content-Type" => "text/html"}, [User.all.to_json]]
     else
       [404, {"Content-Type" => "text/html"}, ["Not Found"]]
     end
