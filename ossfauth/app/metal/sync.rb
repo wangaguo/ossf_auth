4f8bd1e203f05b7a0561f00fd6956f433cc31434
kaworu <kaworu@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-04-08 08:42:18 +0000

    change to .json

--- ossfauth/app/metal/sync.rb
+++ ossfauth/app/metal/sync.rb
@@ -3,8 +3,8 @@ require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Ra
 require 'active_record'
 class Sync
   def self.call(env)
-    if env["PATH_INFO"] =~ /^\/sso\/sync\.js/
-      [200, {"Content-Type" => "text/html"}, [User.all.to_json]]
+    if env["PATH_INFO"] =~ /^\/sso\/sync\.json/
+      [200, {"Content-Type" => "application/json"}, [User.all.to_json]]
     else
       [404, {"Content-Type" => "text/html"}, ["Not Found"]]
     end
