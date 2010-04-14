df8943fb1c11948f15ef0606aa100ee8d405431a
kaworu <kaworu@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-04-14 07:53:43 +0000

    update lang selection, login by token, and session initialization

--- ossfauth/app/controllers/application_controller.rb
+++ ossfauth/app/controllers/application_controller.rb
@@ -10,9 +10,11 @@ class ApplicationController < ActionController::Base
   #self.allow_forgery_protection = false
 
   # Scrub sensitive parameters from your log
-  filter_parameter_logging :password
+  filter_parameter_logging :password, :old_password, :password_confirmation, 
+                           :email, :email_confirmation
   layout 'default'
   before_filter :set_locale
+  #before_filter :set_session
  
   include ActiveMessaging::MessageSender
   def publish_with_yaml(body, header = {}, timeout = 10)
@@ -24,11 +26,50 @@ class ApplicationController < ActionController::Base
 
   private
   def set_locale  
-    @locales = ['English', '繁體中文']
+    @locales = {:en => 'English', :zh_TW => '繁體中文'}
+    locale = ( params[:lang] || cookies[:lang] || session[:lang] || 
+        scan_lang_from_browser )
+    locale = :en unless @locales.has_key? locale.to_sym
+    I18n.locale = locale
+    session[:lang] = I18n.locale
+  end
+  
+  def scan_lang_from_browser
+    lang = 
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    lang = 'zh_TW' if lang == 'zh'
+    lang
+  end
+  
+  def set_session 
+    unless session[:z]
+      session[:z] = 'z'
+      redirect_to "/sso#{request.path}"
+      return false
+    end
+    true
   end
 
   def login_require
-    @user = check_user
-    redirect_to login_user_path unless @user
+    @user = check_user 
+    @event = login_by_token unless @user
+    if @event 
+      redirect_to @event.action
+    else
+      redirect_to login_user_path unless @user
+    end
+  end
+  
+  def login_by_token
+    e = User.authenticate_by_token(params[:t])
+    return nil unless e
+    @user = e.user
+    session[:user] = @user
+    s = e.user.sessions.new
+    s.session_key = request.session_options[:id]
+    s.save!
+    #e.user.eval e.body
+    #redirect_to e.action
+    return e
   end
 end
