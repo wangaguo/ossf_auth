0d55a2cb233bd036aabe1819aaad835c5aced65e
hyder <hyder@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-04-15 03:54:53 +0000

    Add user_notify model function.

---
+++ ossfauth/app/models/user_notify.rb
@@ -0,0 +1,61 @@
+class UserNotify < ActionMailer::Base
+
+  def setup_email(user)
+    @recipients = "#{user.email}"
+    @from       = "contact@openfoundry.org"
+    @subject    = "[OpenFoundry] "
+    @sent_on    = Time.now
+  end
+ 
+  def signup(user, password, url=nil)
+    setup_email(user)
+
+    # Set Contenti-Type for Sending mails
+    @content_type = "text/html"
+
+    # Email header info
+    @subject += "Welcom to OpenFoundry"
+
+    # Email body substitutions
+    @body["name"] = "#{user.first_name} " + "#{user.last_name} "
+    @body["login"] = "#{user.name}"
+    @body["password"] = "#{password}"
+    @body["url"] = url || "http://ssodev.openfoundry.org/"
+    @body["app_name"] = "OpenFoundry"
+  end
+
+  def forgot_password(user, url=nil)
+    setup_email(user)
+
+    # Set Contenti-Type for Sending mails
+    @content_type = "text/html"
+
+    # Email header info
+    @subject += "Forgotten password notification"
+
+    # Email body substitutions
+    @body["name"] = "#{user.first_name} " + "#{user.last_name}"
+    @body["login"] = "#{user.name}"
+    @body["url"] = url || "http://ssodev.openfoundry.org/"
+    @body["app_name"] = "OpenFoundry"
+  end
+
+  def change_email(user, url) 
+    setup_email(user)
+
+    @recipients = user.new_email
+
+    # Set Contenti-Type for Sending mails
+    @content_type = "text/html"
+
+    # Email header info
+    @subject += "Changed email notification"
+
+    # Email body substitutions
+    @body["name"] = "#{user.first_name} " + "#{user.last_name}"
+    @body["new_email"] = user.new_email
+    @body["url"] = url || "http://ssodev.openfoundry.org/"
+    @body["app_name"] = "OpenFoundry"
+  end
+end
+ 
