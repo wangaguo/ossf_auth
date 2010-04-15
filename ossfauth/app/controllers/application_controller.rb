fd7f8e136b563b59be27767c7fe87f60478b56a7
kaworu <kaworu@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-04-15 03:04:52 +0000

    bug fix, http_accept_lang is nil; add comments

--- ossfauth/app/controllers/application_controller.rb
+++ ossfauth/app/controllers/application_controller.rb
@@ -14,7 +14,6 @@ class ApplicationController < ActionController::Base
                            :email, :email_confirmation
   layout 'default'
   before_filter :set_locale
-  #before_filter :set_session
  
   include ActiveMessaging::MessageSender
   def publish_with_yaml(body, header = {}, timeout = 10)
@@ -26,35 +25,40 @@ class ApplicationController < ActionController::Base
 
   private
   def set_locale  
+    #what language we support
     @locales = {:en => 'English', :zh_TW => '繁體中文'}
+   
+    #this is our language selection priority:
     locale = ( params[:lang] || cookies[:lang] || session[:lang] || 
-        scan_lang_from_browser )
-    locale = :en unless @locales.has_key? locale.to_sym
-    I18n.locale = locale
-    session[:lang] = I18n.locale
+        scan_lang_from_browser || :zh_TW )
+
+    #lang is not supported, use :zh_TW
+    locale = :zh_TW unless @locales.has_key? locale.to_sym
+
+    #set lang to session, cookie, and I18n
+    I18n.locale = session[:lang] = cookies[:lang] = locale
   end
   
   def scan_lang_from_browser
-    lang = 
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-    lang = 'zh_TW' if lang == 'zh'
-    lang
-  end
-  
-  def set_session 
-    unless session[:z]
-      session[:z] = 'z'
-      redirect_to "/sso#{request.path}"
-      return false
-    end
-    true
+    ( request.env['HTTP_ACCEPT_LANGUAGE'] || '' ).scan(/^[a-z]{2}/).first
   end
 
   def login_require
+    #the user from session
     @user = check_user 
-    @event = login_by_token unless @user
-    if @event 
-      redirect_to @event.action
+    #the user form security token
+    @event = login_by_token #unless @user
+    if @event
+      if @event.user == @user 
+        redirect_to @event.action
+      else
+        #session user != token user
+        #force session user to logout!
+        #redirect to logout, return_url = request.url
+        redirect_to :controller => :user, :action => :logout, :return_url => request.url      
+      end
+      #expire the token
+      @event.expire!
     else
       redirect_to login_user_path unless @user
     end
@@ -68,8 +72,7 @@ class ApplicationController < ActionController::Base
     s = e.user.sessions.new
     s.session_key = request.session_options[:id]
     s.save!
-    #e.user.eval e.body
-    #redirect_to e.action
+    @user.instance_eval e.body, __FILE__, __LINE__ 
     return e
   end
 end
