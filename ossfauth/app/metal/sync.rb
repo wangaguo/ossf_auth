bafdfcc06149512d529cd3bed29c3c362304d089
kaworu <kaworu@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-04-19 03:54:08 +0000

    sync with timestamp with river

--- ossfauth/app/metal/sync.rb
+++ ossfauth/app/metal/sync.rb
@@ -1,10 +1,17 @@
 # Allow the metal piece to run in isolation
 require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Rails)
-require 'active_record'
+require 'rubygems'
+require 'active_support'
+
 class Sync
   def self.call(env)
     if env["PATH_INFO"] =~ /^\/sso\/sync\.json/
-      [200, {"Content-Type" => "application/json"}, [User.all.to_json]]
+      if req = Rack::Request.new(env) and req.post? and req.params[ "synctime" ]
+        syncuser = User.find( :all, :conditions => [ "updated_at > (?)", Time.at( req.params[ "synctime" ].to_i ) ] )
+        [200, {"Content-Type" => "application/json"}, [syncuser.to_json]]
+      else
+        [200, {"Content-Type" => "application/json"}, [Array.new.to_json]]
+      end
     else
       [404, {"Content-Type" => "text/html"}, ["Not Found"]]
     end
