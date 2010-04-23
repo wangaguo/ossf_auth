7817b5a182cedebb3b0baf90593590df88ef5c44
wangaguo <wangaguo@gmail.com>
2010-04-23 05:23:03 +0000

    1.prototype array problem. 2.adjust code.

--- ossfauth/public/javascripts/langsync.js
+++ ossfauth/public/javascripts/langsync.js
@@ -5,26 +5,26 @@ if(!$sso) var $sso = {};
   sso.langs = ['en', 'zh_TW']; 
   
   sso.createCookie = function(name,value,days) {
+    var expires = '';
     if (days) {
       var date = new Date();
       date.setTime(date.getTime()+(days*24*60*60*1000));
-      var expires = "; expires="+date.toGMTString();
+      expires = "; expires="+date.toGMTString();
     }
-    else var expires = "";
     document.cookie = name+"="+value+expires+"; path=/";
-  }
+  };
 
   sso.getLang = function() {
     var lang = '';
     lang = location.search.match(/[&|?]lang=([^&]*)/,'');
     if(!lang) lang=""; else lang=lang[1]; 
-    for (var i in this.langs)
+    for (var i=0; i<this.langs.length; i++)
     {
       if(location.pathname.match("/"+this.langs[i]+"/")) return(this.langs[i]); 
       if(lang == this.langs[i]) return(this.langs[i]);
     }
     return(""); 
-  }
+  };
   
   sso.langSync = function(lang) {
     if(this.langs.indexOf(lang) < 0) return(0);
@@ -34,11 +34,11 @@ if(!$sso) var $sso = {};
       this.createCookie(this.langCookies[l], lang, '');
     }
     return(1);
-  }
+  };
   
   sso.langAutoSync = function() {
     this.langSync(this.getLang());
-  }
+  };
 })($sso);
 
 $sso.langAutoSync();
