f07c31b20db3c12524cc13b18d2c601e1c8ec2c0
kaworu <kaworu@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-04-27 01:35:11 +0000

    add message n-1 site relation

--- ossfauth/app/models/message.rb
+++ ossfauth/app/models/message.rb
@@ -1,6 +1,16 @@
 class Message < ActiveRecord::Base
   belongs_to :user
+  belongs_to :site
   serialize :body
+
+  def clone
+    m = Message.new
+    %w{action status body user_id site_id}.each{|a|
+      m.write_attribute a, self.read_attribute(a)
+    }
+    m
+  end
+
   named_scope :un_sync, :conditions => {:status => nil}
   named_scope :re_sync, :conditions => {:status => 're_sync'}
 end
--- ossfauth/app/models/site.rb
+++ ossfauth/app/models/site.rb
@@ -1,3 +1,4 @@
 class Site < ActiveRecord::Base
   has_many :urls
+  has_many :messages
 end
