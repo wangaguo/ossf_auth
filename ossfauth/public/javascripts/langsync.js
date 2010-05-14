e517985f4ec54251f1d644b212c0e28dc550f639
wangaguo <wangaguo@gmail.com>
2010-05-14 02:37:06 +0000

    1. Add tw convert to zh_TW. 2. Change joomla cookie name. 3. Change of & sso cookie name to "oflang". (because joomla conflict with "lang", "lang" will be remove.) 4. lang setting: /tw & /tw/ & /en & /en/

--- ossfauth/public/javascripts/langsync.js
+++ ossfauth/public/javascripts/langsync.js
@@ -1,8 +1,8 @@
 if(!$sso) var $sso = {};
 
 (function(sso){
-  sso.langCookies = {openfoundry:'lang', wsw:'mbfcookie[lang]'};
-  sso.langs = ['en', 'zh_TW']; 
+  sso.langCookies = {openfoundry:'oflang', wsw:'jfcookie[lang]'};
+  sso.langs = {en:'en', zh_TW:'zh_TW', tw:'zh_TW'}; 
   
   sso.createCookie = function(name,value,days) {
     var expires = '';
@@ -18,16 +18,16 @@ if(!$sso) var $sso = {};
     var lang = '';
     lang = location.search.match(/[&|?]lang=([^&]*)/,'');
     if(!lang) lang=""; else lang=lang[1]; 
-    for (var i=0; i<this.langs.length; i++)
+    for (var l in this.langs)
     {
-      if(location.pathname.match("/"+this.langs[i]+"/")) return(this.langs[i]); 
-      if(lang == this.langs[i]) return(this.langs[i]);
+      if(location.pathname.match("^/"+l+"(/|$)")) return(this.langs[l]);
+      if(lang == l) return(this.langs[l]); 
     }
     return(""); 
   };
   
   sso.langSync = function(lang) {
-    if(this.langs.indexOf(lang) < 0) return(0);
+    if(!this.langs[lang]) return(0);
 
     for (var l in this.langCookies)
     {
