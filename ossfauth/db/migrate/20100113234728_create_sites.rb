131c4c253071551b0ebd3186cc8358b6bd92428d
kaworu <kaworu@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-07-07 02:35:49 +0000

    refine site sync config

--- ossfauth/db/migrate/20100113234728_create_sites.rb
+++ ossfauth/db/migrate/20100113234728_create_sites.rb
@@ -10,7 +10,7 @@ class CreateSites < ActiveRecord::Migration
 
     Site.create!(:name => 'wsw', :ip => '140.109.22.20', 
         :regist_key => 'e3052d90-f7d1-012c-c171-0011254f08ff')
-    Site.create!(:name => 'of', :ip => '140.109.22.15', 
+    Site.create!(:name => 'of', :ip => '140.109.22.15:3000', 
         :regist_key => 'c1cac710-030f-012d-c173-0011254f08ff')
   end
 
