27274ccb39ae2ce1749b45c5d3eeff8798bf9dc8
kaworu <kaworu@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-08-10 07:59:31 +0000

    add msg methods(not used);maybe some day

--- ossfauth/lib/of.rb
+++ ossfauth/lib/of.rb
@@ -3,3 +3,39 @@ class OpenFoundryError < RuntimeError; end
 
 class SignUpDuplicationError < OpenFoundryError 
 end
+
+require 'yaml'
+
+module OpenFoundry
+  module Message
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    # for deliver ACTIONS, like CRUD
+    ACTIONS = {
+      :create => 'create',
+      :update => 'update',
+      :delete => 'delete'}.freeze
+
+    # for delivery TYPE, like object type
+    TYPES = {
+      :project => 'project',
+      :user => 'user' ,
+      :function => 'function'}.freeze
+
+    module ClassMethods
+      include ActiveMessaging::MessageSender
+
+      publishes_to :ossf_msg
+      def send_msg(type, action, data)
+        publish(:ossf_msg,
+          "#{YAML::dump({'type' => type, 'action' => action, 'data' => data})}"
+        )
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.send(:include, OpenFoundry::Message)
+
