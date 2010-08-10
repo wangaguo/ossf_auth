380e1e2e8dc27cb6496707d8783b606e3841e02b
kaworu <kaworu@358ba581-17c5-de11-b4ea-000bcdefe56b>
2010-08-10 07:58:28 +0000

    disable msg, again

--- ossfauth/config/messaging.rb
+++ ossfauth/config/messaging.rb
@@ -7,7 +7,8 @@ ActiveMessaging::Gateway.define do |s|
   #s.filter :some_filter, :only=>:orders
   #s.processor_group :group1, :order_processor
   
-  s.destination :ossf_message, '/queue/ossf_message'
-  s.destination :joomla_message, '/queue/joomla_message'
+  #s.destination :ossf_message, '/queue/ossf_message'
+  #s.destination :joomla_message, '/queue/joomla_message'
+  s.destination :ossf_msg, '/topic/OSSF.MSG'
   
 end
